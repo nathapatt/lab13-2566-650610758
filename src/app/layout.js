e70286f61945c17ad0072e73e7e05703550dd19e
Mind <nathapat.mind@gmail.com>
2023-08-07 17:35:16 +0700

    finish laaaa

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Nathapat Nerangsi"
+            studentId="650610758"
           />
         </div>
       </body>
